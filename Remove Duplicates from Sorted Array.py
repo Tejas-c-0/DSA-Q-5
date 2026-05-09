6c8e990838d7f0e78ea635b5c6b702cf6254cb53
Tejas C <shobha2ganesh@gmail.com>
2026-05-09 15:23:01 +0530

    Replace reverse function with duplicate check

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -1,10 +1,11 @@
-s = ["h","e","l","l","o"]
-def reverse(s):
-    left, right = 0, len(s) - 1
-    while left < right:
-        s[left], s[right] = s[right], s[left]
-        left += 1
-        right -= 1
-        
-    return s
-print(reverse(s)) 
+nums = [1,2,3,1]
+k = 3
+def containsNearbyDuplicate(nums, k):    
+    hashmap = {}
+    for i, num in enumerate(nums):
+        if num in hashmap:
+            if i - hashmap[num] <= k:
+                return True
+        hashmap[num] = i
+    return False
+print(containsNearbyDuplicate(nums,k))  
